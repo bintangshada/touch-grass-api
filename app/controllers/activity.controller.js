d83ea71d31e910c816def3c43bb6cbb9a46c598b
bintangshada <bintangshada45@gmail.com>
2023-08-10 23:32:46 +0700

    fix weather issues and make default response route

--- app/controllers/activity.controller.js
+++ app/controllers/activity.controller.js
@@ -5,16 +5,58 @@ require("dotenv").config();
 
 exports.randomActivity = async (req, res) => {
   try {
-    const city = req.body.city;
-    const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`);
+    const city = req.params.city;
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPENWEATHER_API_KEY}`
+    );
     const weather = weatherResponse.data.weather[0].main;
+    const weatherDesc = weatherResponse.data.weather[0].description;
+    const temperatureKelvin = weatherResponse.data.main.temp;
+
+    const temperature = (temperatureKelvin - 273.15).toFixed(0) + "°C";
 
     const activities = await Activity.findAll({
       where: { weather: weather },
       attributes: {
-        exclude: ["id","createdAt","updatedAt"],
-      }
+        exclude: ["id", "createdAt", "updatedAt"],
+      },
     });
+
+    if (
+      [
+        "Thunderstrom",
+        "Squall",
+        "Haze",
+        "Dust",
+        "Fog",
+        "Sand",
+        "Ash",
+        "Tornado",
+      ].includes(weather)
+    ) {
+      return res.status(200).send({
+        message: "I think stay at home is not that bad",
+        status: 200,
+      });
+    }
+
+    if (
+      [
+        "ragged shower rain",
+        "sleet",
+        "heavy snow",
+        "heavy intensity shower rain",
+        "freezing rain",
+        "extreme rain",
+        "very heavy rain",
+      ].includes(weatherDesc)
+    ) {
+      return res.status(200).send({
+        message: "It's up to you now, the weather isn't that good, right?",
+        status: 200,
+      });
+    }
+
 
     if (activities.length === 0) {
       return res.status(404).send({
@@ -29,6 +71,7 @@ exports.randomActivity = async (req, res) => {
     return res.status(200).send({
       message: randomData,
       status: 200,
+      temperature: temperature,
     });
   } catch (err) {
     console.error(err);
@@ -37,3 +80,82 @@ exports.randomActivity = async (req, res) => {
     });
   }
 };
+
+
+exports.defaultResponse = async (req,res) => {
+  try {
+    const weatherResponse = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Jakarta&appid=${process.env.OPENWEATHER_API_KEY}`
+    );
+    const weather = weatherResponse.data.weather[0].main;
+    const weatherDesc = weatherResponse.data.weather[0].description;
+    const temperatureKelvin = weatherResponse.data.main.temp;
+
+    const temperature = (temperatureKelvin - 273.15).toFixed(0) + "°C";
+
+    const activities = await Activity.findAll({
+      where: { weather: weather },
+      attributes: {
+        exclude: ["id", "createdAt", "updatedAt"],
+      },
+    });
+
+    if (
+      [
+        "Thunderstrom",
+        "Squall",
+        "Haze",
+        "Dust",
+        "Fog",
+        "Sand",
+        "Ash",
+        "Tornado",
+      ].includes(weather)
+    ) {
+      return res.status(200).send({
+        message: "I think stay at home is not that bad",
+        status: 200,
+      });
+    }
+
+    if (
+      [
+        "ragged shower rain",
+        "sleet",
+        "heavy snow",
+        "heavy intensity shower rain",
+        "freezing rain",
+        "extreme rain",
+        "very heavy rain",
+      ].includes(weatherDesc)
+    ) {
+      return res.status(200).send({
+        message: "It's up to you now, the weather isn't that good, right?",
+        status: 200,
+      });
+    }
+
+
+    if (activities.length === 0) {
+      return res.status(404).send({
+        message: "No data available",
+        status: 404,
+      });
+    }
+
+    const randomIndex = Math.floor(Math.random() * activities.length);
+    const randomData = activities[randomIndex];
+
+    return res.status(200).send({
+      message: randomData,
+      status: 200,
+      temperature: temperature,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({
+      message: err.message,
+      status: 500,
+    })
+  }
+}
--- app/routes/v1/activity.route.js
+++ app/routes/v1/activity.route.js
@@ -2,7 +2,8 @@ module.exports = (app) => {
     const activityController = require("../../controllers/activity.controller");
     const router = require("express").Router();
   
-    router.get("/", activityController.randomActivity);
+    router.get("/:city", activityController.randomActivity);
+    router.get("/", activityController.defaultResponse);
   
     app.use("/api/v1/activity", router);
   };
